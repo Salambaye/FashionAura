33417764c055047bcb05538343b1c3903da4c121
Sala <Salambaye2001@gmail.com>
2024-09-28 18:21:32 +0200

    Header

--- src/comp/nav.js
+++ src/comp/nav.js
@@ -1,11 +1,15 @@
 import React from 'react'
-import { MdLocalShipping } from "react-icons/md";
+import { MdLocalShipping } from 'react-icons/md'
+import './nav.css'
 const Nav = () => {
     return (
         <div className='header'>
             <div className='top_header'>
                 <div className='icon'>
-
+                    <MdLocalShipping />
+                </div>
+                <div className='info'>
+                    <p>Livraison gratuite pour toute première commande</p>
                 </div>
             </div>
         </div>
